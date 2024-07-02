5aeef3b220e80f490397e70c9cc4ea4024e90e9b
jeady <jeadyu@outlook.com>
2024-07-02 17:19:48 +0800

    sonatype uploader sample for tag 2.4

--- greetingApi/build.gradle.kts
+++ greetingApi/build.gradle.kts
@@ -1,8 +1,10 @@
+import io.github.jeadyx.UploaderSigning
+
 plugins {
     id("java-library")
     alias(libs.plugins.jetbrains.kotlin.jvm)
     id("org.jetbrains.dokka") version "1.9.20"
-    id("io.github.jeadyx.sonatype-uploader") version "2.3"
+    id("io.github.jeadyx.sonatype-uploader") version "2.4"
 }
 
 group = "io.github.jeady5"
@@ -11,33 +13,38 @@ java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
 }
-apply{ from("publish.gradle") }
-//val tokenUser: String by project
-//val tokenUserPasswd: String by project
-//sonatypeUploader {
-//    tokenName = tokenUser
-//    tokenPasswd = tokenUserPasswd
-//    pom = Action<MavenPom>{
-//        name = "My Library"
-//        description = "A concise description of my library greennbg"
-//        url = "http://www.example111.com/library"
-//        licenses {
-//            license {
-//                name = "The Apache License, Version 2.0"
-//                url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
-//            }
-//        }
-//        developers {
-//            developer {
-//                id = "jeady"
-//                name = "jeady"
-//                email = "jeady@example.com"
-//            }
-//        }
-//        scm {
-//            connection = "scm:git:git://example.com/my-library.git"
-//            developerConnection = "scm:git:ssh://example.com/my-library.git"
-//            url = "http://example.com/my-library/"
-//        }
-//    }
-//}
+//apply{ from("publish.gradle") }
+val tokenUser: String by project
+val tokenUserPasswd: String by project
+sonatypeUploader {
+    tokenName = tokenUser
+    tokenPasswd = tokenUserPasswd
+    signing = Action<UploaderSigning> {
+        keyId="9EAFF062"
+        keyPasswd="123123"
+        secretKeyPath="E:\\test_0x9EAFF062_SECRET.gpg"
+    }
+    pom = Action<MavenPom>{
+        name = "My Library"
+        description = "A concise description of my library greennbg"
+        url = "http://www.example111.com/library"
+        licenses {
+            license {
+                name = "The Apache License, Version 2.0"
+                url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+            }
+        }
+        developers {
+            developer {
+                id = "jeady"
+                name = "jeady"
+                email = "jeady@example.com"
+            }
+        }
+        scm {
+            connection = "scm:git:git://example.com/my-library.git"
+            developerConnection = "scm:git:ssh://example.com/my-library.git"
+            url = "http://example.com/my-library/"
+        }
+    }
+}
